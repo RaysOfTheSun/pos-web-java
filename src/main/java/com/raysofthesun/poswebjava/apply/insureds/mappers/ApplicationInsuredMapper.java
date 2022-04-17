0d77401f940c58c374fb585d6713bb60a3b6ea11
Carl Rayos <rdelsol35@gmail.com>
2022-04-17 18:08:35 +0800

    bugfix(apply): disable automatic mapping of ApiCustomer id to InsuredId to keep Insured ids uniuqe

--- src/main/java/com/raysofthesun/poswebjava/apply/insureds/mappers/ApplicationInsuredMapper.java
+++ src/main/java/com/raysofthesun/poswebjava/apply/insureds/mappers/ApplicationInsuredMapper.java
@@ -2,14 +2,16 @@ package com.raysofthesun.poswebjava.apply.insureds.mappers;
 
 import com.raysofthesun.poswebjava.apply.feign.models.ApiCustomer;
 import com.raysofthesun.poswebjava.apply.insureds.models.core.insured.Insured;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
+
+import java.util.UUID;
 
 @Mapper
 public interface ApplicationInsuredMapper {
 	ApplicationInsuredMapper MAPPER = Mappers.getMapper(ApplicationInsuredMapper.class);
 
-	@Mapping(source = "id", target = "customerId")
+    @Mapping(source = "id", target = "id", ignore = true)
+    @Mapping(source = "id", target = "customerId")
 	Insured mapApiCustomerToInsured(ApiCustomer apiCustomer);
 }
